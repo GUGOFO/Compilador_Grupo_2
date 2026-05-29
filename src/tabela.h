656aab646c2a3f42635b5c82cdd85dffa61b120a
Vplayer-coder <vin326d156@gmail.com>
2026-05-29 15:51:28 -0300

    Passando a tabela de símbolos integrada com o parser para main.

---
+++ src/tabela.h
@@ -0,0 +1,18 @@
+#ifndef TABELA_SIMBOLOS_H
+#define TABELA_SIMBOLOS_H
+
+typedef struct Simbolo{
+    char nome[36];
+    char tipo[20];
+    int escopo;
+
+    struct Simbolo *prox;
+} Simbolo;
+
+void inserirSimbolo(char *nome, char *tipo, int escopo);
+Simbolo *buscarSimbolo(char *nome, int escopo);
+void removerEscopo(int escopo_alvo);
+void imprimirTabela();
+
+
+#endif
